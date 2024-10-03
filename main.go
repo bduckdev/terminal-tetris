1d67722a4851410894d096f1b44545fcfb761d80
Brennan Duck <brennantduck@gmail.com>
2024-10-03 14:50:12 -0400

    updated readme

--- main.go
+++ main.go
@@ -1,18 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+type model struct {
+	score int
+	lines int
+	level int
+}
+
+var style = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#FAFAFA")).
+	Background(lipgloss.Color("#7D56F4")).
+	PaddingTop(2).
+	PaddingLeft(4).
+	Width(22)
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return "cool text"
+}
+
 func main() {
-	daysSpentDucking := 0
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatalln("err: ", err)
+	}
+	defer f.Close()
 
-	fmt.Println("Days spent ducking: ", daysSpentDucking)
-
-	for {
-		time.Sleep(24 * time.Hour)
-		daysSpentDucking++
-		fmt.Println("Days spent ducking: ", daysSpentDucking)
+	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
 }
